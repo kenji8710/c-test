41012d7aae759f9446cc25aacdb34910c79b2122
三河賢持 <mikawakenji@mikawakenmochinoMacBook-Air.local>
2024-04-29 09:04:08 +0900

    C言語の練習

---
+++ hello.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("What is X?\n");
+    scanf("%d",&x);
+    printf("What is Y?\n");
+    scanf("%d",&y);
+    z=x+y;
+    printf("X+Y= %d\n" , z);
+}
